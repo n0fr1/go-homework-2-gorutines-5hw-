aae71e3389d70039114039b948fd2fa584b1d341
Evgeniy <n0fr1@mail.ru>
2021-05-04 13:03:20 +0300

    add 1 exercise

---
+++ hw5/flows/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+
+	var (
+		counter int
+		mutex   sync.Mutex
+		count   = 1000
+		wg      = sync.WaitGroup{}
+	)
+
+	wg.Add(count)
+	for i := 0; i < count; i += 1 {
+
+		go func(id int) {
+			defer wg.Done()
+
+			fmt.Printf("Start № %v\n", id)
+
+			mutex.Lock()
+			counter += 1
+			mutex.Unlock()
+
+			fmt.Printf("Stop № %v\n", id)
+
+		}(i)
+	}
+
+	wg.Wait()
+
+	fmt.Printf("\n")
+	fmt.Printf("Counter: %v", counter)
+
+}
